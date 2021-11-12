312010a4ffca4902d2b567f601f99e21f251df92
BRIAN WIGGINS <64331187+Bigwiggz@users.noreply.github.com>
2021-11-11 19:18:38 -0500

    Update site.js

--- js/site.js
+++ js/site.js
@@ -6,6 +6,19 @@
 let startValue=parseFloat(document.getElementById("startValue").value);
 let endValue=parseFloat(document.getElementById("endValue").value);
 
+//
+var tbodyRef = document.getElementById('myTable').getElementsByTagName('tbody')[0];
+
+// Insert a row at the end of table
+var newRow = tbodyRef.insertRow();
+
+// Insert a cell at the end of the row
+var newCell = newRow.insertCell();
+
+// Append a text node to the cell
+var newText = document.createTextNode('new row');
+newCell.appendChild(newText);
+
 function ValidateDataEntry(startValue, endValue){
 
     if(startValue>endValue){
@@ -14,7 +27,11 @@ function ValidateDataEntry(startValue, endValue){
     else{
         
         for(let i=startValue; i<endValue; i++){
-
+            if(i%2==0){
+                class="gray";
+            }
+            else{
+            }
         }
     }
 }
